393c01c3220229cebcab7736f4c7f915c6d99ec3
Logan Pashby <pashbylogan@gmail.com>
2023-12-09 16:00:07 -0700

    day 8 part 2 solved

--- 2023/src/day08.py
+++ 2023/src/day08.py
@@ -1,4 +1,5 @@
 from typing import Dict, Tuple, TextIO
+from math import gcd
 
 def p1(file):
     answer = 0
@@ -33,23 +34,23 @@ def p2(file: TextIO) -> int:
         left, right = step.strip().replace('(', '').replace(')', '').split(', ')
         node_map[start] = (left, right)
 
-    cur_nodes: list[str] = [key for key in node_map.keys() if key[-1] == 'A']
-    while True:
-        print(answer, cur_nodes)
-        if instructions[answer % len(instructions)] == 'R':
-            cur_nodes = [node_map[cur_node][1] for cur_node in cur_nodes]
-        else:
-            cur_nodes = [node_map[cur_node][0] for cur_node in cur_nodes]
+    starting_nodes: list[str] = [key for key in node_map.keys() if key[-1] == 'A']
+    cycle_lengths = [0] * len(starting_nodes)
 
-        answer += 1
+    for i, cur_node in enumerate(starting_nodes):
+        while cur_node[-1] != 'Z':
+            if instructions[answer % len(instructions)] == 'R':
+                cur_node = node_map[cur_node][1]
+            else:
+                cur_node = node_map[cur_node][0]
+            cycle_lengths[i] += 1
+            answer += 1
 
-        if all([cur_node[-1] == 'Z' for cur_node in cur_nodes]):
-            break
+    lcm = cycle_lengths[0]
+    for i in cycle_lengths[1:]:
+        lcm = lcm * i // gcd(lcm, i)
 
-        if answer > 10:
-            break
-
-    return answer
+    return lcm
 
 
 with open('inputs/d8') as file:
